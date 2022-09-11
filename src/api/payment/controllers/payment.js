2e80531f7437a351e0ac79e2ec883546853c86d4
Oka Wibawa <okaa.wibawa@gmail.com>
2022-09-12 03:50:38 +0800

    xendit payment services

--- src/api/payment/controllers/payment.js
+++ src/api/payment/controllers/payment.js
@@ -4,12 +4,73 @@
  *  payment controller
  */
 
+const Xendit = require("xendit-node");
+const { createAuthorization } = require("xendit-node/src/card/authorization");
+const x = new Xendit({
+  secretKey:
+    "xnd_development_URrf17t6hhUNJTzSR7cxGpQSnhKirmLQndQehhKsqNeycsy1ZJoFFQDjLaxAO",
+  xenditURL: "https://api.xendit.co",
+});
+const { VirtualAcc } = x;
+
+const va = new VirtualAcc({});
+
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::payment.payment", ({ strapi }) => ({
-  async xenditCallback(ctx) {
-    console.log({ ctx });
+  async xenditVa() {
+    const banks = await va.getVABanks();
 
+    const entities = banks.filter((bank) => bank.is_activated === true);
+
+    return entities;
+  },
+
+  async xenditCreateVa(ctx) {
+    const { body } = ctx.request;
+    const {
+      body: { productId },
+    } = ctx.request;
+
+    const fixedVA = await va.createFixedVA(body);
+
+    ctx.request.body = {
+      data: {
+        transaction: null,
+        is_paid: false,
+        xendit_va_object: fixedVA,
+      },
+    };
+
+    const payment = await super.create(ctx);
+
+    ctx.request.body = {
+      data: {
+        status: "waiting_payment",
+        payment: payment.data.id,
+        profile: null,
+      },
+    };
+
+    const transaction = await strapi
+      .controller("api::transaction.transaction")
+      .create(ctx);
+
+    ctx.request.body = {
+      data: {
+        product: productId,
+        transaction: transaction.data.id,
+      },
+    };
+
+    const transaction_detail = await strapi
+      .controller("api::transaction-detail.transaction-detail")
+      .create(ctx);
+
+    return payment;
+  },
+
+  async xenditCallback(ctx) {
     return ctx.send(
       {
         message: "Body received",
--- src/api/payment/routes/custom-payment.js
+++ src/api/payment/routes/custom-payment.js
@@ -4,6 +4,22 @@ module.exports = {
   routes: [
     {
       method: "POST",
+      path: "/xendit/create-va",
+      handler: "payment.xenditCreateVa",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "GET",
+      path: "/xendit/get-va",
+      handler: "payment.xenditVa",
+      config: {
+        policies: [],
+      },
+    },
+    {
+      method: "POST",
       path: "/payment/callback-xendit",
       handler: "payment.xenditCallback",
       config: {
