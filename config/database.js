940ddc12215dfa8ae774df62fa7c4bbf579abe34
okawibawa <okaa.wibawa@gmail.com>
2022-10-13 23:28:24 +0800

    ratings service

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "seafood-putra"),
+      database: env("DATABASE_NAME", "seafood_putra"),
       user: env("DATABASE_USERNAME", "root"),
       password: env("DATABASE_PASSWORD", ""),
     },
